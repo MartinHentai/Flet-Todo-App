254c5215888c52e6f96540344ba5010cd7719888
MartinHentai <Martinohentai11@gmail.com>
2026-03-15 10:49:16 +0300

    fixed most of bugs in drag.py, exept fading of last dropped item when hovered

--- bin/Declarative/drag.py
+++ bin/Declarative/drag.py
@@ -2,11 +2,34 @@ import flet as ft
 
 
 @ft.component
-def drag(i, num, func):
+def drag(i, num, func, last_droped):
     width= 800
     height= 50
     is_dragged, set_drag_over = ft.use_state(False)
+    is_hovered, handle_hover = ft.use_state(False)
+    is_droped, set_droped = ft.use_state(False)
 
+    def switch_is_droped():
+        print("HALLO")
+        last_droped = None
+
+    set_droped(num == last_droped)
+
+    
+
+
+
+    def render_hover(e):
+        handle_hover(e.data == True)
+
+        
+
+    
+    
+
+    bg_color = ft.Colors.WHITE10 if ( is_hovered) else ft.Colors.TRANSPARENT
+    
+    
 
     def click(e):
         print(e)
@@ -18,57 +41,73 @@ def drag(i, num, func):
 
     def accept(e):
         set_drag_over(False)
-        print("SOURCE :", e.src.content.title)
-        print("Dragged item id", e.src.data)
-        print("DESTINATION :", e.control.content.controls[1].content.title)
-        print("DESTINATION I :", i)
-
+        handle_hover(False)
         func(old_index=e.src.data,new_index=i)
 
     def leave(e):
         set_drag_over(False)
-
+        handle_hover(False)
     
+    def set_opacity(e, n):
+        e.control.parent.parent.parent.parent.opacity = n
 
 
 
     return ft.DragTarget(
-        expand=1,
-        
+        expand=1,     
         on_will_accept=will_accept,
         on_leave=leave,
         on_accept=accept,
-        
         content=ft.Column(
             spacing=0,
             controls=[
                 ft.Container(
                     height=50 if is_dragged else 0,
-                    animate=ft.Animation(300,ft.AnimationCurve.EASE_IN_OUT),
+                    animate=ft.Animation(200,ft.AnimationCurve.EASE_IN_OUT),
                     bgcolor=ft.Colors.TRANSPARENT,
                 ),
                 ft.Draggable( 
-                    data=i,
-                    content=ft.ListTile(
-                        trailing=ft.IconButton(icon=ft.Icons.ABC, on_click=click),
-                        expand=1,
-                        title=(f"Item {num}"),
-                        bgcolor="#999999",
-                        height= 50,
+                    data=i,  
+                    content=ft.Container(
+                        height=40,
+                        width=width,
+                        border_radius = ft.BorderRadius.all(50),
+                        padding = ft.Padding.only(left=10, right=10),
+                        on_hover= render_hover,
+                        bgcolor=bg_color,
+                        content=ft.AnimatedSwitcher(
+                            duration=600 if is_droped  else 0,
+                            reverse_duration=0,
+                            on_animation_end=lambda e: set_droped(False),
+                            transition=ft.AnimatedSwitcherTransition.FADE , 
+                            switch_in_curve=ft.AnimationCurve.EASE_IN_CUBIC,
+                            content=ft.Row(
+                                alignment=ft.MainAxisAlignment.START,
+                                controls=[
+                                    ft.Icon(ft.Icons.ABC, size=20, color=ft.Colors.WHITE70), 
+                                    ft.Text(value=(f"Item {num}"), size=14, expand=True, color=ft.Colors.WHITE),             
+                                ],
+                            )
+                        )
                     ),
-                    
                     content_when_dragging=ft.Container(height=0),
                     content_feedback=ft.Container(
-                        bgcolor="#ff0000",
-                        width= 800,
-                        height= 50,
-                        content=ft.ListTile(
-                            title=(f"Item {num}"),
-                            bgcolor="#FF0000",
+                        height=40,
+                        width= width,
+                        border_radius = ft.BorderRadius.all(50),
+                        padding = ft.Padding.only(left=10, right=10),
+                        on_hover= render_hover,
+                        bgcolor=bg_color,
+                        content=ft.Row(
+                            alignment=ft.MainAxisAlignment.START,
+                            controls=[
+                                ft.Icon(ft.Icons.ABC, size=20, color=ft.Colors.WHITE70), 
+                                ft.Text(value=(f"Item {num}"), size=14, expand=True, color=ft.Colors.WHITE),             
+                            ],
                         )
+                    ),
                     
 
-                    )
                 )
             ]
         )
@@ -82,14 +121,18 @@ def drag(i, num, func):
 def drags():
 
     items, set_items = ft.use_state(list(range(20)))
+    last_droped, set_droped = ft.use_state(None)
 
     def reorder(old_index, new_index):
         copy = items.copy()
         if old_index < new_index:
-            new_index = new_index -1
+            new_index -= 1
         moved_item = copy.pop(old_index)
         copy.insert(new_index, moved_item)
+        set_droped(moved_item)
         set_items(copy)
+
+    
 
 
 
@@ -98,9 +141,10 @@ def drags():
     return ft.Container(
         expand=1,
         content=ft.ListView(
+            # opacity=0.5,
             expand=1,
             spacing=3,
-            controls=[drag(i=i, num=num, func=reorder) for i, num in enumerate(items)]
+            controls=[drag(i=i, num=num, func=reorder,last_droped=last_droped) for i, num in enumerate(items)]
         )
     )
 
